d34696c45497c87341dc39acb53b9f9070321b91
Wilco Louwerse <wilco@conduction.nl>
2020-07-10 15:45:10 +0200

    Fixed domain name in conductionFixtures

--- api/src/DataFixtures/ConductionFixtures.php
+++ api/src/DataFixtures/ConductionFixtures.php
@@ -28,8 +28,7 @@ class ConductionFixtures extends Fixture
             // If build all fixtures is true we build all the fixtures
             !$this->params->get('app_build_all_fixtures') &&
             // Specific domain names
-            $this->params->get('app_domain') != 'zuiddrecht.nl' && strpos($this->params->get('app_domain'), 'zuiddrecht.nl') == false &&
-            $this->params->get('app_domain') != 'zuid-drecht.nl' && strpos($this->params->get('app_domain'), 'zuid-drecht.nl') == false
+            $this->params->get('app_domain') != 'conduction.nl' && strpos($this->params->get('app_domain'), 'conduction.nl') == false
         ) {
             return false;
         }
