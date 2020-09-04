85cc1dff882bcd18ca81c2c806460dd53ade49b1
ginokok1996 <ginokok1996@hotmail.com>
2020-09-04 10:49:41 +0200

    added file upload to onboaring

--- api/src/DataFixtures/CheckinFixtures.php
+++ api/src/DataFixtures/CheckinFixtures.php
@@ -72,7 +72,6 @@ class CheckinFixtures extends Fixture
         $processType->addStage($stage);
         $manager->persist($processType);
         $manager->flush();
-
         $section = new Section();
         $section->setStage($stage);
         $section->setName('Onderneming');
@@ -81,6 +80,7 @@ class CheckinFixtures extends Fixture
             $this->commonGroundService->cleanUrl(['component'=>'vtc', 'type'=>'properties', 'id'=>'55dde78d-4a14-43c6-a0ff-d33b7b5f8bae']),
             $this->commonGroundService->cleanUrl(['component'=>'vtc', 'type'=>'properties', 'id'=>'9e5c34dc-99da-423d-9a88-a4a3875a66fb']),
             $this->commonGroundService->cleanUrl(['component'=>'vtc', 'type'=>'properties', 'id'=>'41122a46-4788-4ba1-aba9-b48f7f640ef8']),
+            $this->commonGroundService->cleanUrl(['component'=>'vtc', 'type'=>'properties', 'id'=>'2abb52b1-bf30-4359-a027-fede87b63f64']),
         ]);
         $stage->addSection($section);
 
