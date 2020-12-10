d16c7041d08a533c09debb0861acac1c811c9e72
ginokok1996 <ginokok1996@hotmail.com>
2020-12-10 13:40:28 +0100

    fixture updates

--- api/src/DataFixtures/UtrechtFixtures.php
+++ api/src/DataFixtures/UtrechtFixtures.php
@@ -52,8 +52,10 @@ class UtrechtFixtures extends Fixture
         $stage->setIcon('fal fa-users');
         $stage->setSlug('huwelijk-ceremonie');
         $stage->setDescription('Als je gaat trouwen moet je veel regelen. Om je wat overzicht te geven hebben we hieronder een lijstje gemaakt van alles wat je met de Gemeente moet regelen. We leggen het ook uit in een  filmpje.');
+        $stage->setProcess($processType);
 
         $section = new Section();
+        $section->setStage($stage);
         $section->setName('Soort huwelijk');
         $section->setDescription('Jullie moeten een keuze maken of jullie een huwelijk of een geregistreerd partnerschap willen
                     afsluiten. Als je nog niet weet welke keuze je moet maken kan je op de site van de <a
@@ -63,6 +65,7 @@ class UtrechtFixtures extends Fixture
         $stage->addSection($section);
 
         $section = new Section();
+        $section->setStage($stage);
         $section->setName('Soort Ceremonie');
         $section->setDescription('Als jullie gaan trouwen zijn moet je kiezen tussen gratis, eenvoudig en ceremonieel trouwen. Dit
                     bepaalt hoeveel keuze je hebt bij het kiezen van je locatie, trouwdatum en trouwambtenaren.');
@@ -70,20 +73,25 @@ class UtrechtFixtures extends Fixture
         $stage->addSection($section);
 
         $section = new Section();
+        $section->setStage($stage);
         $section->setName('Partner');
         $section->setDescription('Met wie wilt u trouwen');
         $section->setProperties([$this->commonGroundService->cleanUrl(['component' => 'vtc', 'type' => 'properties', 'id' => '8357401c-0551-4f75-891f-a0c7f4b72d41'])]);
         $stage->addSection($section);
 
         $processType->addStage($stage);
+        $manager->persist($processType);
+        $manager->flush();
 
         $stage = new Stage();
         $stage->setName('Ambtenaar en Locatie');
         $stage->setIcon('fal fa-users');
         $stage->setSlug('ambtenaar-locatie');
         $stage->setDescription('De keuze in trouwlocatie is afhankelijk van het type huwelijk (gratis, eenvoudig of ceremonieel). Als het type huwelijk de mogelijkheid biedt om een locatie te kiezen, is het nodig dat jullie aangeven  waar jullie het huwelijk willen laten voltrekken.');
+        $stage->setProcess($processType);
 
         $section = new Section();
+        $section->setStage($stage);
         $section->setName('Ambtenaar');
         $section->setDescription('De trouwambtenaar is degene die het huwelijk met jullie gaat voltrekken. De keuze in trouwambtenaar
                     is afhankelijk van het type huwelijk (gratis, eenvoudig of ceremonieel). Als het type huwelijk de
@@ -94,11 +102,15 @@ class UtrechtFixtures extends Fixture
         $stage->addSection($section);
 
         $section = new Section();
+        $section->setStage($stage);
         $section->setName('Locatie');
         $section->setDescription('De keuze in trouwlocatie is afhankelijk van het type huwelijk (gratis, eenvoudig of ceremonieel). Als het type huwelijk de mogelijkheid biedt om een locatie te kiezen, is het nodig dat jullie aangeven  waar jullie het huwelijk willen laten voltrekken.');
         $section->setProperties([$this->commonGroundService->cleanUrl(['component' => 'vtc', 'type' => 'properties', 'id' => '5908ddf4-a221-405f-a981-02b76aeeae58'])]);
-
         $stage->addSection($section);
+
+        $processType->addStage($stage);
+        $manager->persist($processType);
+        $manager->flush();
 
         $stage = new Stage();
         $stage->setName('Wanneer wilt u trouwen?');
@@ -106,8 +118,10 @@ class UtrechtFixtures extends Fixture
         $stage->setSlug('Jullie moeten kiezen wanneer je bij de Gemeente de plechtigheid wil laten voltrekken. De beschikbare
                     data zijn afhankelijk van het type ceremonie (gratis, eenvoudig of ceremonieel) en optioneel de ambtenaar of locatie');
         $stage->setDescription('Wie treed op als belanghebbende?');
+        $stage->setProcess($processType);
 
         $section = new Section();
+        $section->setStage($stage);
         $section->setName('Datum');
         $section->setDescription('Jullie moeten kiezen wanneer je bij de Gemeente de plechtigheid wil laten voltrekken. De beschikbare
                     data zijn afhankelijk van het type ceremonie (gratis, eenvoudig of ceremonieel) en optioneel de ambtenaar of locatie.');
@@ -115,40 +129,50 @@ class UtrechtFixtures extends Fixture
         $stage->addSection($section);
 
         $processType->addStage($stage);
+        $manager->persist($processType);
+        $manager->flush();
 
         $stage = new Stage();
         $stage->setName('Getuigen');
         $stage->setIcon('fal fa-users');
         $stage->setSlug('getuigen');
         $stage->setDescription('Bij het trouwen of geregistreerde partnerschap is het wettelijk verplicht dat er minimaal twee getuigen aanwezig zijn, één voor elke partner. Er zijn maximaal 4 getuigen toegestaan bij Gemeente Utrecht.');
+        $stage->setProcess($processType);
 
         $section = new Section();
+        $section->setStage($stage);
         $section->setName('Getuigen');
         $section->setDescription('Trouwen of partnerschap');
         $section->setProperties([$this->commonGroundService->cleanUrl(['component' => 'vtc', 'type' => 'properties', 'id' => '02d72f6c-cacb-4f41-a54f-f735c6dab1f6'])]);
         $stage->addSection($section);
 
         $processType->addStage($stage);
+        $manager->persist($processType);
+        $manager->flush();
 
         $stage = new Stage();
         $stage->setName('Overige gegevens');
         $stage->setIcon('fal fa-users');
         $stage->setSlug('Er zijn natuurlijk ook een aantal andere vragen waarvan we van u een antwoord moeten weten');
         $stage->setDescription('Wie treed op als belanghebbende?');
+        $stage->setProcess($processType);
 
         $section = new Section();
+        $section->setStage($stage);
         $section->setName('Naamsgebruik');
         $section->setDescription('Welke naam wenst u te gebruiken');
         $section->setProperties([$this->commonGroundService->cleanUrl(['component' => 'vtc', 'type' => 'properties', 'id' => 'ebbeed6e-2023-4b71-a6c7-31fcaa4fe3a6'])]);
         $stage->addSection($section);
 
         $section = new Section();
+        $section->setStage($stage);
         $section->setName('Taal');
         $section->setDescription('taal van de plechtigheid');
         $section->setProperties([$this->commonGroundService->cleanUrl(['component' => 'vtc', 'type' => 'properties', 'id' => 'fcdbd0db-4cf2-48e1-aef4-16291ebb4b35'])]);
         $stage->addSection($section);
 
         $section = new Section();
+        $section->setStage($stage);
         $section->setName('Extras');
         $section->setDescription('Het is mogelijk om extra opties te kiezen om je ceremonie extra feestelijk te maken, zoals
                     bijvoorbeeld een trouwboekje als aandenken aan het huwelijk.');
@@ -156,24 +180,29 @@ class UtrechtFixtures extends Fixture
         $stage->addSection($section);
 
         $section = new Section();
+        $section->setStage($stage);
         $section->setName('Opmerkingen');
         $section->setDescription('Trouwen of partnerschap');
         $section->setProperties([$this->commonGroundService->cleanUrl(['component' => 'vtc', 'type' => 'properties', 'id' => '025f4395-b90f-434f-91a4-a19bcf9de83d'])]);
         $stage->addSection($section);
 
         $section = new Section();
+        $section->setStage($stage);
         $section->setName('Melding voorgenomen huwelijk');
         $section->setDescription('Voordat je gaat trouwen of een geregistreerd partnerschap aangaat is het wettelijk verplicht een melding voorgenomen huwelijk te gaan doen. Dit moet minimaal 14 dagen voordat de ceremonie voltrokken wordt.');
         $section->setProperties([$this->commonGroundService->cleanUrl(['component' => 'vtc', 'type' => 'properties', 'id' => '05db9c53-cf96-47d9-926f-0dac80cd3d61'])]);
         $stage->addSection($section);
 
         $section = new Section();
+        $section->setStage($stage);
         $section->setName('Betaling');
         $section->setDescription('Tenzij je voor een gratis huwelijk hebt gekozen moet je betalen voor je huwelijk.');
         $section->setProperties([$this->commonGroundService->cleanUrl(['component' => 'vtc', 'type' => 'properties', 'id' => 'f6231405-373e-4477-9026-265db00601e1'])]);
         $stage->addSection($section);
 
         $processType->addStage($stage);
+        $manager->persist($processType);
+        $manager->flush();
 
         // Save it all to the db
         $manager->persist($processType);
